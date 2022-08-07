5fbb92f809200ba96041bfbaedb8cf5af26f9018
savrixx <stevenvillaflor@gmail.com>
2022-08-06 22:26:19 -0500

    !

---
+++ curl/curl-curl-7_84_0/packages/OS400/__tmpsrcf.c
@@ -0,0 +1,7 @@
+#line 1 "os400.c"
+#pragma convert(819)
+#line 1
+#pragma comment(user, "libcurl version ")
+#pragma comment(user, __DATE__)
+#pragma comment(user, __TIME__)
+#pragma comment(copyright, "Copyright (C) 1998-2016 Daniel Stenberg et al. OS/400 version by P. Monnerat")
---
+++ curl/curl-curl-7_84_0/packages/OS400/os400.c
@@ -0,0 +1,4 @@
+#pragma comment(user, "libcurl version ")
+#pragma comment(user, __DATE__)
+#pragma comment(user, __TIME__)
+#pragma comment(copyright, "Copyright (C) 1998-2016 Daniel Stenberg et al. OS/400 version by P. Monnerat")
